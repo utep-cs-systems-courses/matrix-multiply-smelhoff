bd48ec1621c016d319c72dd4310d7dae736da234
smelhoff <smelhoff@gmail.com>
2021-09-14 13:42:41 -0600

    Changed back to args for input, updated instructions

--- matrixUtils.py
+++ matrixUtils.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import time
 
@@ -62,7 +63,7 @@ def multiMatrix(a, b):                                                  # requir
     for i in range(length):                                             # rows
         for j in range(length):                                         # columns
             for k in range(length):                                     # index
-                matrix[i][j] = a[i][k] * b[k][j]                        # multplication result
+                matrix[i][j] += a[i][k] * b[k][j]                       # multplication result
     return matrix                                                       #result
 
 def multiMatrixBlocked(a, b):
@@ -78,13 +79,35 @@ def multiMatrixBlocked(a, b):
                     k_end = kk + tile_size
                     sum = matrix[i][j]
                     for k in range(kk, k_end):
-                        sum = a[i][k] * b[k][j]
+                        sum = sum + a[i][k] * b[k][j]
                     matrix[i][j] = sum
     return matrix
 
 def main():
     """
     Used for running as a script
+    """
+    parser = argparse.ArgumentParser(description=
+    'Generate a 2d matrix and save it to  a file.')
+    parser.add_argument('-s', '--size', default=256, type=int,
+    help='Size of the 2d matrix to generate')
+    parser.add_argument('-v', '--value', default=1, type=int,
+    help='The value with which to fill the array with')
+    parser.add_argument('-f', '--filename',
+    help='The name of the file to save the matrix in (optional)')
+
+    args = parser.parse_args()
+
+    mat = genMatrix(args.size, args.value)
+    mat2 = genMatrix(args.size, args.value * 2)
+
+    if args.filename is not None:
+        print(f'Writing matrix to {args.filename}')
+        writeToFile(mat, args.filename)
+
+        print(f'Testing file')
+        printSubarray(readFromFile(args.filename))
+
     """
     size = int(input("Enter the size of the matrix to generate: "))
     if size < 10: size = 10
@@ -93,18 +116,26 @@ def main():
 
     matA = genMatrix(size, valueA)
     matB = genMatrix(size, valueB)
+    """
 
     start = time.monotonic()                    # time before computation
-    matC = multiMatrix(matA, matB)
+    mat3 = multiMatrix(mat, mat2)
     elapsed = time.monotonic() - start          # time taken to complete
 
     start = time.monotonic()
-    matC = multiMatrixBlocked(matA, matB)
+    mat3 = multiMatrixBlocked(mat, mat2)
     elapsed2 = time.monotonic() - start
 
-    printSubarray(matC)
-    print("Matrix multiply time: %s seconds" % elapsed)
-    print("Matrix multiply blocked time: %s seconds" % elapsed2)
+    #matE = np.dot(mat, mat2)                   # used for testing
+
+    print("Matrix 1:")
+    printSubarray(mat)
+    print("Matrix 2:")
+    printSubarray(mat2)
+    print("Matrix 3 (result):")
+    printSubarray(mat3)
+    print("Matrix multiply time: %.4f seconds" % elapsed)
+    print("Matrix multiply blocked time: %.4f seconds" % elapsed2)
 
 if __name__ == '__main__':
     # execute only if run as a script
